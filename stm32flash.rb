75cea4bb96766e04ea256739e761e9a3e8049170
Christopher Hwang <chris@silpstream.com>
2014-02-26 20:15:06 +0800

    Update to 20140225 version of stm32flash

--- stm32flash.rb
+++ stm32flash.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Stm32flash < Formula
   homepage "https://code.google.com/p/stm32flash/"
-  version "20131218"
-  url "https://gitorious.org/stm32flash/stm32flash/archive/8399fbe1baf2b7d097746786458021d92895d71b.tar.gz"
+  version "20140225"
+  url "https://gitorious.org/stm32flash/stm32flash/archive/7af2d66ad29c156176c6f62b1045cd354294b12a.tar.gz"
   sha1 "80c8683b6221941b59cc372cc827140211fde973"
 
   def install
