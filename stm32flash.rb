a31fd2bedb9a17d5ca05f535bb006f53319d197b
Christopher Hwang <chris@silpstream.com>
2014-07-25 19:23:49 +0800

    no message

--- stm32flash.rb
+++ stm32flash.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Stm32flash < Formula
   homepage "https://code.google.com/p/stm32flash/"
-  version "20140328"
-  url "https://gitorious.org/stm32flash/stm32flash/archive/2e04bf684eaeb7a737597e010e13b98b617397f7.tar.gz"
-  sha1 "b6d9e70eb64249819a76ed8615c02a60034f73c8"
+  version "20140715"
+  url "https://gitorious.org/stm32flash/stm32flash/archive/f26ab723e7c11cff6e10ebd76541d1a62e56f965.tar.gz"
+  sha1 "177406adac142c1c5bbe45fbb6eebd8caa96a2f1"
 
   def install
     system "make"
