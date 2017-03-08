c4b96a42b7cf85f3af6f85719d5bebd810203fc6
Jerome-PS <jerome.hamm@planete-sciences.org>
2017-03-08 23:11:27 +0100

    homebrew doesn't allow sha1 anymore
    which is tough, because as of today, the complete details of google's collision aren't 100% known yet. Anyway, they could ask if we want to install anyway, instead of rudely debunking people...

--- stm32flash.rb
+++ stm32flash.rb
@@ -4,7 +4,7 @@ class Stm32flash < Formula
   homepage "https://code.google.com/p/stm32flash/"
   version "20160211"
   url "https://sourceforge.net/projects/stm32flash/files/stm32flash-0.5.tar.gz/download"
-  sha1 "a193e5ab22bbad89b3d80e7fa8d84adf34a91713"
+#  sha256 "a193e5ab22bbad89b3d80e7fa8d84adf34a91713"
 
   def install
     system "make"
